e70d379031eac30b5b7daedbc6bbdebf721a1b2c
guruprince8 <guru.prince8@gmail.com>
2022-05-08 19:00:41 +0530

    message

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by C302318 on 08/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
